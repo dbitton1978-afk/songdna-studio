55a4b9d1a2d08e0c3450c9640d4885273a1cd594
dbitton1978-afk <dbitton1978@gmail.com>
2026-04-21 19:31:28 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import dnaRoutes from "./routes/dnaRoutes.js";
+import variationRoutes from "./routes/variationRoutes.js";
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use("/uploads", express.static("uploads"));
 
 app.use("/api/upload", uploadRoutes);
 app.use("/api/dna", dnaRoutes);
+app.use("/api/variation", variationRoutes);
 
 app.get("/health", (req, res) => {
   res.json({ success: true });
