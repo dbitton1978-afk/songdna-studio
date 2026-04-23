fab523381e1f6cb5786db3bbc683a725514cfda4
dbitton1978-afk <dbitton1978@gmail.com>
2026-04-23 16:54:07 +0300

    Update analyze.py

--- server/python/analyze.py
+++ server/python/analyze.py
@@ -7,18 +7,14 @@ file_path = sys.argv[1]
 
 y, sr = librosa.load(file_path)
 
-# BPM
 tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
 
-# Energy
 rms = librosa.feature.rms(y=y)[0]
 energy = float(np.mean(rms))
 
-# Spectral centroid (brightness)
 centroid = librosa.feature.spectral_centroid(y=y, sr=sr)[0]
 brightness = float(np.mean(centroid))
 
-# Zero crossing rate (rhythm complexity)
 zcr = librosa.feature.zero_crossing_rate(y)[0]
 rhythm = float(np.mean(zcr))
 
